5aab14c24d33297a5037cb3cffc789813c394e40
Nathan Shaw <n.shaw@entando.com>
2022-09-21 17:46:20 -0400

    Enable context parameters

--- microfrontends/simple-mfe/src/App.js
+++ microfrontends/simple-mfe/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App({config}) {
-  const { params } = config || {};
+  const { contextParams, params} = config || {};
   const { name } = params || {};
 
   return (
@@ -10,6 +10,13 @@ function App({config}) {
           <p>
             Hello, {name}!
           </p>
+            { contextParams && (
+                <>
+                  <div>Page Code: {contextParams.page_code}</div>
+                  <div>Current Language: {contextParams.info_currentLang}</div>
+                  <div>Application Base URL: {contextParams.systemParam_applicationBaseURL}</div>
+                </>
+            )}
         </header>
       </div>
   );
